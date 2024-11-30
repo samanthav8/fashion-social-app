3b6f369eb88649097575b2b655c99304b2527f05
Samantha <samanthavillafana8@gmail.com>
2024-11-29 20:42:03 -0500

    create components

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Fashion Social App</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/channels">Channels</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
